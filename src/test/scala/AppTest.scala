0d21241da38db3a198d603080c23537729e60d59
mikhailmelnikifmo <mihail.melnik.ifmo@gmail.com>
2015-10-12 17:40:12 +0200

    maven structure

---
+++ src/test/scala/AppTest.scala
@@ -0,0 +1,12 @@
+import org.junit.Test
+import org.junit.Assert._
+/**
+ * Created by Mishanya on 12.10.2015.
+ */
+@Test
+class AppTest {
+  @Test
+  def testOK() {
+    assertTrue("It shall be done", true)
+  }
+}
