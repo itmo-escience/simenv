527f53349cb94f751b4d4ce9f406d79ace64753e
mikhailmelnikifmo <mihail.melnik.ifmo@gmail.com>
2015-12-21 12:12:41 +0300

    merge from mike_branch

--- src/main/scala/itmo/escience/Environment/Entities/Workflow.scala
+++
@@ -1,8 +0,0 @@
-package itmo.escience.Environment.Entities
-
-/**
-  * Created by user on 02.11.2015.
-  */
-class Workflow(name:String, headTask:HeadTask) {
-
-}
--- src/main/scala/itmo/escience/Environment/Tag.scala
+++
@@ -1,8 +0,0 @@
-package itmo.escience.Environment
-
-/**
-  * Created by Mishanya on 24.11.2015.
-  */
-class Tag {
-  var tag: Int = 666
-}
