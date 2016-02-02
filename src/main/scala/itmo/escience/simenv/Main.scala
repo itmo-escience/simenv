60dc7f08df8414f80037e81e0bbb85f9bf76f7d5
HighExecutor <mihail.melnik.ifmo@mail.ru>
2016-02-02 21:30:09 +0300

    PABEDA

--- src/main/scala/itmo/escience/simenv/Main.scala
+++ src/main/scala/itmo/escience/simenv/Main.scala
@@ -14,48 +14,44 @@ import itmo.escience.simenv.utilities.Units._
 object Main {
   def main(args: Array[String]) {
     val wfName = "Montage_50"
-    val wfPath = ".\\resources\\wf-examples\\" + wfName
+    val wfNames = List("Montage_25", "Montage_50", "CyberShake_30", "CyberShake_50", "Inspiral_30", "Inspiral_50")
+    val downTimes = List(0, 5, 10, 15, 20)
+    val wfPath = ".\\resources\\wf-examples\\"
+    val expPath = ".\\temp\\exps\\"
     //  val basepath = ".\\resources\\"
     //  val wf_name = "crawlerWf"
-
-    val nodeResizeTime = 1
-    val nodeDownTime = 5
-    val resDownTime = 10
-
-    val expPath = ".\\temp\\exps\\"
-//    val cgaFile: PrintWriter = new PrintWriter(expPath + "CGA2_" + wfName + "_down_" + nodeDownTime + ".txt", "UTF-8")
-//    val gaFile: PrintWriter = new PrintWriter(expPath + "GA_" + wfName + "_down_" + nodeDownTime + ".txt", "UTF-8")
-//    val heftFile: PrintWriter = new PrintWriter(expPath + "HEFT_" + wfName + "_down_" + nodeDownTime + ".txt", "UTF-8")
-
     val envArray = List(List(10.0, 10.0, 20.0), List(10.0, 10.0, 20.0))
-//    val envArray = List(List(30.0, 30.0))
-//    val envArray = List(List(0.0, 0.0, 0.0, 20.0))
 
     val globNet = 10 Mbit_Sec
-//    val globNet = 100 Kbit_Sec
-//    val locNet = 1000 Kbit_Sec
     val locNet = 1000 Mbit_Sec
     val reliability = 0.95
-    println("2")
-    for (i <- 0 until 10) {
-      println("---------")
-      println("CGA exp:")
-      val cgaRes = new CGADynamExp(wfPath, envArray, globNet, locNet, reliability, nodeResizeTime, nodeDownTime, resDownTime).run()
-//      println("GA exp:")
-//      val gaRes = new GADynamExp(wfPath, envArray, globNet, locNet, reliability, nodeResizeTime, nodeDownTime, resDownTime).run()
-//      println("HEFT exp:")
-//      val heftRes = new HEFTDynamExp(wfPath, envArray, globNet, locNet, reliability, nodeResizeTime, nodeDownTime, resDownTime).run()
 
-//      println("GA exp:")
-//      val gaRes = new GAStaticExp(wfPath, envArray, globNet, locNet, reliability).run()
-//      println("HEFT exp:")
+    for (downTime <- downTimes) {
+      for (wf <- wfNames) {
 
-//      cgaFile.write(cgaRes + "\n")
-//      gaFile.write(gaRes + "\n")
-//      heftFile.write(heftRes + "\n")
+        val cgaFile: PrintWriter = new PrintWriter(expPath + "CGA_" + wfName + "_down_" + downTime + ".txt", "UTF-8")
+        val gaFile: PrintWriter = new PrintWriter(expPath + "GA_" + wfName + "_down_" + downTime + ".txt", "UTF-8")
+        val heftFile: PrintWriter = new PrintWriter(expPath + "HEFT_" + wfName + "_down_" + downTime + ".txt", "UTF-8")
+
+        for (i <- 0 until 10) {
+          println("---------")
+          println("CGA exp:")
+          val cgaRes = new CGADynamExp(wfPath + wfName, envArray, globNet, locNet, reliability, 0, downTime, 0).run()
+          println("GA exp:")
+          val gaRes = new GADynamExp(wfPath + wfName, envArray, globNet, locNet, reliability, 0, downTime, 0).run()
+          println("HEFT exp:")
+          val heftRes = new HEFTDynamExp(wfPath + wfName, envArray, globNet, locNet, reliability, 0, downTime, 0).run()
+
+          cgaFile.write((cgaRes + "\n").replace(".", ","))
+          gaFile.write((gaRes + "\n").replace(".", ","))
+          heftFile.write((heftRes + "\n").replace(".", ","))
+        }
+
+        cgaFile.close()
+        gaFile.close()
+        heftFile.close()
+
+      }
     }
-//    cgaFile.close()
-//    gaFile.close()
-//    heftFile.close()
   }
 }
